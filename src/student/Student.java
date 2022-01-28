2d4ad5ac99c6304efdb6ee6440b27962fa5e9ea3
villafas <98172751+villafas@users.noreply.github.com>
2022-01-28 09:48:38 -0500

    Added author comments

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+Author:Sebastian Villafane
+Date: Jan/28/2022
  */
 package student;
 
