673e55acfe0e458773b64649a96cb0179bcfba6c
Sebastian <villafas@sherdiancollege.ca>
2022-01-28 10:26:16 -0500

    part-time-student class added

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author sebas
+ */
+public class PartTimeStudent extends Student {
+    
+    private int numCourses;
+    
+    public PartTimeStudent(String studentId, String studentName, int numCourses){
+        super(studentId, studentName);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+    
+    
+}
